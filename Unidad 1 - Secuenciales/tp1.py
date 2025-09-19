235ac36ca99e5754053b99dae1af4aeea6a2dd3d
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2025-09-19 05:05:53 -0300

    Unidad 1

---
+++ Unidad 1 - Secuenciales/tp1.py
@@ -0,0 +1,68 @@
+# 1
+print("Hola Mundo!")
+
+# 2
+# nombre = input("Ingrese su Nombre: ")
+# print(f"Hola {nombre}!")
+
+# 3
+nombre = input("Ingrese su Nombre: ")
+apellido = input("Ingrese su Apellido: ")
+edad = input("Ingrese su Edad: ")
+residencia = input("Ingrese su Lugar de residencia: ")
+
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+# 4
+radio = float(input("Ingrese el radio del círculo: "))
+print(
+    f"El área del círculo es {3.14 * (radio ** 2)} y su perímetro es {3.14 * (radio * 2)}")
+
+# 5
+segundos = int(input("Ingrese la cantidad de segundos: "))
+print(f"{segundos} equivalen a {segundos / 3600} horas")
+
+# 6
+numero = int(input("Ingrese un número: "))
+print(f"""
+   {numero} x 1 = {numero * 1}
+   {numero} x 2 = {numero * 2}
+   {numero} x 3 = {numero * 3}
+   {numero} x 4 = {numero * 4}
+   {numero} x 5 = {numero * 5}
+   {numero} x 6 = {numero * 6}
+   {numero} x 7 = {numero * 7}
+   {numero} x 8 = {numero * 8}
+   {numero} x 9 = {numero * 9}
+   {numero} x 10 = {numero * 10}
+""")
+
+# 7
+numero = int(input("Ingrese un número entero distinto a 0: "))
+otroNumero = int(input("Ingrese otro número entero distinto a 0: "))
+
+print(f"""
+       {numero} + {otroNumero} = {numero + otroNumero}
+       {numero} / {otroNumero} = {numero / otroNumero}
+       {numero} * {otroNumero} = {numero * otroNumero}
+       {numero} - {otroNumero} = {numero - otroNumero}
+""")
+
+# 8
+altura = float(input("Ingrese su altura en metros: "))
+peso = float(input("Ingrese su peso en kg: "))
+
+print(f"Su índice de masa corporal es: {peso / (altura**2)}")
+
+# 9
+temperatura = float(input("Ingrese la temperatura en grados Celcius: "))
+
+print(f"La temperatura en Fahrenheit es: {(9/5) * temperatura + 32}")
+
+# 10
+numero = int(input("Ingrese un número: "))
+numeroDos = int(input("Ingrese otro número: "))
+numeroTres = int(input("Ingrese otro número: "))
+
+print(
+    f"El promedio de los 3 números es {int((numero + numeroDos + numeroTres) / 3)}")
